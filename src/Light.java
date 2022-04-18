bff0126965dde16416779a96c73ca2349778ae64
Ziggy18 <91980874+Ziggy18@users.noreply.github.com>
2022-04-18 11:07:05 +0300

    Update Light.java

--- src/Light.java
+++ src/Light.java
@@ -4,11 +4,6 @@ public class Light {
     // цвет света. для получения яркости [0,1] необходимо каждую компоненту разделить на 255
     private Color color;
 
-    public Light(){
-        this.direction=new Coordinate(0,0,1);
-        this.color=new Color(255, 255, 255);
-    }
-
     public Light(Coordinate direction, Color color){
         this.direction=direction;
         this.color=color;
@@ -18,15 +13,7 @@ public class Light {
         return direction;
     }
 
-    public void setDirection(Coordinate direction) {
-        this.direction = direction;
-    }
-
     public Color getColor() {
         return color;
-    }
-
-    public void setColor(Color color) {
-        this.color = color;
     }
 }
